4647577b56df3279705ed25963648c92581e6db2
Kate <KhovanskayaK@yandex.ru>
2023-07-28 10:42:09 +0300

    реализован механизм сортировки heapsort

---
+++ heapSort.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+
+public class heapSort {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++)
+            array[i] = new Random().nextInt(20);
+        print(array);
+        heapSort(array);
+        print(array);
+    }
+
+    public static void heapSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void heapify(int arr[], int n, int i) {
+        int largest = i; 
+        int left = 2 * i + 1; 
+        int right = 2 * i + 2; 
+
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void print(int[] array) {
+        for (int i = 0; i < array.length; i++)
+            System.out.print(array[i] + " ");
+        System.out.println();
+    }
+}
